1f6e81585ce43f035076814415a341a114559b31
Tarek Belkahia <tbelkahia@gmail.com>
2021-06-13 06:04:39 +0100

    commit from newBranch

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 //package.json create manually in the root
 // npm init
 //npm init -y (default)
+// devDependencies: sudo npm install -D xxxxx
+// nodemon: node listening for changes
 
 const _ = require('lodash');
 
@@ -9,3 +11,4 @@ const items = [1,[2,[3,[4]]]];
 const newItems = _.flattenDeep(items);
 
 console.log(newItems);
+console.log("hello world!!!");
